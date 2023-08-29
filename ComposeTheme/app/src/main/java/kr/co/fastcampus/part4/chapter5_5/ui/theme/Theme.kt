c6513908cf94ba025b6e90f3be793b1e41e81bc0
황신운 <brandy.hwang@gmail.com>
2023-08-29 10:05:44 +0900

    ✨ feat : Theme Example Done

---
+++ ComposeTheme/app/src/main/java/kr/co/fastcampus/part4/chapter5_5/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package kr.co.fastcampus.part4.chapter5_5.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+// 단계 2: `onSurface` 값을 할당합니다.
+// `surface` 값도 할당해봅시다.
+
+// 단계 3: `primary`와 `onPrimary` 값도 바꾸어봅시다.
+private val LightColorPalette = lightColors(
+    primary = Color(0xff497895),
+    onPrimary = Color.White,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    surface = Color(0xff49958c),
+    onSurface = Color.White,
+
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ThemeTestTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = LightColorPalette
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
